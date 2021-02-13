af2d7b8b7dedc8cb049d8c300af1576218b470a0
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-02-13 22:48:23 +0900

    feat: implement run method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 fn main() {
-  let string = String::from("127.0.0.1:8080");
-  let string_slice = &string[10..];
-  let string_borrow: &str = &string;
-  let string_literal = "1234";
-
-  dbg!(&string);
-  dbg!(string_slice);
-  dbg!(string_borrow);
-  dbg!(string_literal);
-
-  // let server = Server::new("127.0.0.1:8080");
-  // server.run();
+  let server = Server::new("127.0.0.1:8080".to_string());
+  server.run();
 }
 
 struct Server {
@@ -22,5 +12,7 @@ impl Server {
     Self { addr }
   }
 
-  fn run(self) {}
+  fn run(self) {
+    println!("Listening on {}", self.addr);
+  }
 }
